2e777a9a7dc370521d54b9e4f95b27d48a3722d6
l3miage-essouaik <hirohito595@gmail.com>
2024-01-23 18:09:44 +0100

    merge

--- src/main.js
+++ src/main.js
@@ -4,14 +4,15 @@ import router from './router';
 import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
 
-
-import SignUpModal from '@/components/modals/SignUpModal.vue';
-app.component('SignUpModalVue', SignUpModal);
 const app = createApp(App);
 app.use(router).use(Vuetify);
 
+import SignUpModal from '@/components/modals/SignUpModal.vue';
+app.component('SignUpModalVue', SignUpModal);
+
 import LoginModal from '@/components/modals/LoginModal.vue';
 app.component('LoginModal', LoginModal);
+
 import FacebookIcon from '@/components/Icons/FacebookIcon.vue';
 app.component('FacebookIcon', FacebookIcon); 
 
