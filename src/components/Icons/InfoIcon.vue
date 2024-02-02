2c70b1dc487c77ea9811f1d5e0e6b9807ebf4587
l3miage-essouaik <hirohito595@gmail.com>
2024-02-02 01:28:13 +0100

    ok

---
+++ src/components/Icons/InfoIcon.vue
@@ -0,0 +1,17 @@
+<template>
+<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M12 8.99999L12 9.00999" stroke="#160042" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+<circle cx="12" cy="12" r="7.25" stroke="#160042" stroke-width="1.5"></circle>
+<path d="M12 15.01V11.51" stroke="#160042" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+</svg>
+
+
+    </template>
+    
+      
+    <script>
+    export default {
+      name: 'InfoIcon',
+    };
+    </script>
+    
